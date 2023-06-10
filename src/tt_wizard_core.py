86ea4b636394319601d6d23370afadb19c288636
Dennis Schweer <dennis@bumblebeeman.engineer>
2023-06-10 18:51:27 +0200

    Renaming
    Renamed "downloadPath" to "penPath". This better reflects the expanded feature set. tt_wizard_core manages the media content on the pen and is more than just a downloader.

--- src/tt_wizard_core.py
+++ src/tt_wizard_core.py
@@ -15,17 +15,17 @@ class tt_wizard_core:
 
     __mediaDict = {}
 
-    __downloadPath = ""
+    __penPath = ""
 
-    def __init__(self, downloadPath = ""):
-        """ __init__(self, downloadPath = "")
+    def __init__(self, penPath = ""):
+        """ __init__(self, penPath = "")
         Contructor.
 
         (optional) param: String. Specifies path to storage location of gme files.
         """
-        if downloadPath is "":
+        if penPath is "":
             self.__findMountPoint()
-        self.__downloadPath = downloadPath
+        self.__penPath = penPath
         self.__mediaDict = {}
         self.__getAvailableMedia(self.__LIST_PATH)
 
@@ -60,7 +60,7 @@ class tt_wizard_core:
 
     def __findMountPoint(self):
         """
-        Tries to find mount point of pen. If mount point is found, __downloadPath is set to mount point path and True returned.
+        Tries to find mount point of pen. If mount point is found, __penPath is set to mount point path and True returned.
 
         return: True -- Bool. Mountpoint found.
                 False -- Bool. Mountpoint NOT found.
@@ -69,18 +69,18 @@ class tt_wizard_core:
         for disk in psutil.disk_partitions():
             mnt = str(disk.mountpoint) + "/"
             if "tiptoi" in mnt.lower():
-                self.__downloadPath = mnt
+                self.__penPath = mnt
                 return True
         return False
 
-    def setPenPath(self, downloadPath = ""):
+    def setPenPath(self, penPath = ""):
         """ 
         Overwrites path to pen / download location. For example, to be used when auto detection fails.
 
-        param: >>downloadPath<< -- String. New path value.
+        param: >>penPath<< -- String. New path value.
         return: None
         """
-        self.__downloadPath = downloadPath
+        self.__penPath = penPath
 
     def getAllAvailableTitles(self):
         """ 
@@ -119,7 +119,7 @@ class tt_wizard_core:
         return: No return value.
         """
         if filePath is None:
-            path = self.__downloadPath
+            path = self.__penPath
         else:
             path = filePath
         for title in fileNameList:
@@ -138,7 +138,7 @@ class tt_wizard_core:
                 FALSE -- Update is NOT required.
         """ 
         if filePath is None:
-            path = self.__downloadPath
+            path = self.__penPath
         else:
             path = filePath
             
@@ -187,7 +187,7 @@ class tt_wizard_core:
         """
         from os import listdir
         if filePath is None:
-            path = self.__downloadPath
+            path = self.__penPath
         else:
             path = filePath
 
